9564e8c0450f82d4a0108a6cdbe469e2cb5b3c75
Aryan810 <67587135+Aryan810@users.noreply.github.com>
2021-09-29 13:14:39 +0530

    Update HelloWorld.py
    #hacktoberfest

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,16 @@
-print('hello world!')
+# Wait for user input to start the programe
+input('Press Enter To start...')
+
+# This will print Hello World 100 times
+import time
+num = 50
+for i in range(num):
+  print(" "*i + "hello world!")
+  time.sleep(0.05)
+for i in range(num):
+  print(" "*(num-i) + "hello world!")
+  time.sleep(0.05)
+
+  
+# This line will wait for user input and allow user to see the printed statements
+input()
